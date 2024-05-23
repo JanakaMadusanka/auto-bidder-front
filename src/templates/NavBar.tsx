44f8297abebbf9a69568f22a42c4d216b478ba4d
janaka <jnk.madusanka86@gmail.com>
2024-05-23 18:33:39 +0530

    NavBar Component added

--- src/templates/NavBar.tsx
+++ src/templates/NavBar.tsx
@@ -1,7 +1,23 @@
-const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
-}
+import React from 'react';
+import logo from '../assets/common/Logo2.png'
 
-export default NavBar
+const NavBar: React.FC = () => {
+    return (
+        <nav className="bg-gray-800 text-white p-4">
+            <div className="container mx-auto flex items-center justify-between">
+                <div className="size-20"><img src={logo} alt="" /></div>
+                <div className="text-[40px] font-bold">Auto Bidder</div>
+                <div className="hidden md:flex space-x-10">
+                    <a href="#" className="hover:text-gray-400">Home</a>
+                    <a href="#" className="hover:text-gray-400">About</a>
+                    <a href="#" className="hover:text-gray-400">Services</a>
+                    <a href="#" className="hover:text-gray-400">Contact</a>
+                    <a href="#" className="hover:text-gray-400">Sign Up</a>
+                    <a href="#" className="hover:text-gray-400">Sign In</a>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
