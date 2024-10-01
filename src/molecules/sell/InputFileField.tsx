8fe703890d237b044d65e77aa47b2abd63b6b430
janaka <jnk.madusanka86@gmail.com>
2024-10-01 16:35:46 +0530

    Vehicle Registration File up-loader added

--- src/molecules/sell/InputFileField.tsx
+++ src/molecules/sell/InputFileField.tsx
@@ -1,14 +1,22 @@
 interface InputFieldProps {
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  uploadFile?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   title: string;
-  classNames?: string
+  classNames?: string;
+  name?:string;
 }
-  const InputFileField=({title,classNames,onChange}: InputFieldProps) => {
+  const InputFileField=({title,classNames,uploadFile,name}: InputFieldProps) => {
   return (
     <div className={`mb-4 ${classNames}`}>
       <label className="block text-gray-700 text-sm font-bold mb-2">{title}</label>
       <div className="relative">
-        <input type="file" className="absolute inset-0 w-full h-full opacity-0 cursor-pointer" onChange={onChange}/>
+        <input 
+          type="file" 
+          className="absolute inset-0 w-full h-full opacity-0 cursor-pointer" 
+          onChange={uploadFile} 
+          name={name}/>                        
+          {/* id={id}
+          multiple={!singleFileUpload} */}
+                                    
         <div className="bg-gray-500 text-white py-2 px-4 rounded-lg shadow-md hover:bg-blue-600">
           Click to upload
         </div>
