444a597e209c4a689a3b1e708307ac1ac37a6c38
janaka <jnk.madusanka86@gmail.com>
2024-11-08 06:18:28 +0530

    Remove my vehicle list and the content added to my vehicle modal

--- src/organisms/sell/MyVehicleModal.tsx
+++ src/organisms/sell/MyVehicleModal.tsx
@@ -1,6 +1,35 @@
-import MyVehicleList from "./MyVehicleList"
+import { useEffect, useState } from "react";
+import { useAuth } from "../../context/AuthContext";
+import VehicleCard from "../../molecules/sell/VehicleCard";
 
 const MyVehicleModal = () => {
+
+  const { loggedUserId } = useAuth(); // Use the context to set the logged user
+
+  const [vehicles, setVehicles] = useState<any[]>([]); // State to handle an array of vehicles
+
+  useEffect(() => {
+    const fetchVehicleData = async () => {
+      if (!loggedUserId) return; // Avoid fetch if loggedUserId is not available
+
+      try {
+        const response = await fetch(`http://localhost:8082/vehicle/search-by-owner/${loggedUserId}`);
+        const result = await response.json();
+
+        // Check the structure of result to make sure it's an array of vehicles, set the vehicles state
+        if (Array.isArray(result)) {
+          setVehicles(result); 
+        } else {
+          console.error('Expected an array, but got:', result);
+        }
+      } catch (error) {
+        console.error("Error fetching vehicle data:", error);
+      }
+    };
+
+    fetchVehicleData();
+  }, [loggedUserId]);
+
 
   return (
     <div>
@@ -9,7 +38,11 @@ const MyVehicleModal = () => {
           <div className="flex justify-center items-center py-4 text-bg-gray-800">
             <p className="text-3xl font-bold">My Vehicles</p>
           </div>
-          <MyVehicleList />
+          <div className="grid grid-cols-1">
+            {vehicles.map((vehicle) => (
+              <VehicleCard key={vehicle.regNo} vehicle={vehicle} /> // Pass each vehicle to VehicleCard
+            ))}
+          </div>
         </div>
       </div>
     </div>
