23019af11ba95306147f7bf0ac1909b20f6778ec
janaka <jnk.madusanka86@gmail.com>
2024-12-04 19:42:55 +0530

    Remove vehicle button function updated

--- src/templates/Sell.tsx
+++ src/templates/Sell.tsx
@@ -4,11 +4,16 @@ import SellHedder from "../organisms/sell/SellHedder";
 import MyVehicle from "../organisms/sell/MyVehicleModal";
 import SetAuctionModal from "../organisms/sell/SetAuctionModal";
 import UpdateVehicleModal from "../organisms/sell/UpdateVehicleModal";
+import UpdateImagesModal from "../organisms/sell/UpdateImagesModal";
+import Swal from "sweetalert2";
+import { useAtom } from "jotai";
+import { selectedVehicleAtom } from "../store/VehicleAtom";
 
 const Sell = () => {
 
   const [MyComponent, setMyComponent] = useState<JSX.Element | null>(null);
   const [MyComponentVisibility, setMyComponentVisibility] = useState(false);
+  const [selectedVehicle] = useAtom(selectedVehicleAtom);
 
   //Handle Set For Auctuon Button On clic
   function setAuctionButtonOnAction() {
@@ -28,17 +33,49 @@ const Sell = () => {
 
   //Handle show images Button On clic
   function showImagesButtonOnAction() {
-    // setMyComponentVisibility(true);
-    // setMyComponent(<UpdateVehicleModal
-    //   backButtonOnAction={myVehiclesButtonOnAction} //Handle back Button of set Auction Modal
-    // />)
+    setMyComponentVisibility(true);
+    setMyComponent(<UpdateImagesModal
+      backButtonOnAction={myVehiclesButtonOnAction} //Handle back Button of set Auction Modal
+    />)
   }
+  
   //Handle remove vehicle Button On clic
   function removeButtonOnAction() {
-    // setMyComponentVisibility(true);
-    // setMyComponent(<UpdateVehicleModal
-    //   backButtonOnAction={myVehiclesButtonOnAction} //Handle back Button of set Auction Modal
-    // />)
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        fetch(`http://localhost:8082/vehicle/delete/${selectedVehicle?.id}`, {
+          method: "DELETE", 
+          headers: {
+            "Content-Type": "application/json", 
+          },
+        })
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Failed to delete the vehicle.");
+            }
+            return response.text();
+          })
+
+          .then(() => {
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success"
+            });
+            myVehiclesButtonOnAction(); // Refresh or update state after successful deletion
+          })
+      }
+    });
+
   }
 
   //Handle My Vehicle Button Click
@@ -46,9 +83,9 @@ const Sell = () => {
     setMyComponentVisibility(true);
     setMyComponent(<MyVehicle
       setAuctionButtonOnAction={setAuctionButtonOnAction}
-      updateButtonOnAction = {updateButtonOnAction}
-      showImagesButtonOnAction = {showImagesButtonOnAction}
-      removeButtonOnAction = {removeButtonOnAction}
+      updateButtonOnAction={updateButtonOnAction}
+      showImagesButtonOnAction={showImagesButtonOnAction}
+      removeButtonOnAction={removeButtonOnAction}
 
     />)
   }
